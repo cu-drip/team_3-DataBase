49d14c57f5b814fa616c5b60190157a7da84a0b5
Enotich Enotov <119893655+EnotichEnotow@users.noreply.github.com>
2025-07-04 00:12:18 +0300

    Delete новая практика directory

--- новая практика/repo-root/infra/db/init/01_roles.sql
+++
@@ -1,8 +0,0 @@
--- Контейнер уже создаёт супер-роль `user` (env POSTGRES_USER).
--- Просто назначим ей все привилегии на схему, чтобы Flyway и Spring жили спокойно.
-
-GRANT ALL PRIVILEGES ON ALL TABLES    IN SCHEMA public TO "user";
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "user";
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-      GRANT ALL PRIVILEGES ON TABLES TO "user";
